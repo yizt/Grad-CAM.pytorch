1a4e0bbb7079a755da7ada47d918e5fc2921bb63
yizt <csuyzt@163.com>
2019-08-04 17:50:32 +0800

    修复bug

--- interpretability/guided_back_propagation.py
+++ interpretability/guided_back_propagation.py
@@ -7,63 +7,28 @@ Created on 2019/8/4 上午9:45
 """
 import torch
 from torch import nn
-from torch.autograd import Function
 import numpy as np
 
 
-class _GuidedBackPropagationReLUFunction(Function):
-    """
-    定义Function实现自定义的forward和backward
-    """
-
-    def forward(self, x):
-        output = torch.clamp(x, min=0)
-        self.save_for_backward(x, output)
-        return output
-
-    def backward(self, grad_output):
-        x, _ = self.saved_tensors
-        forward_mask = (x > 0).type_as(x)
-        backward_mask = (grad_output > 0.).type_as(grad_output)
-        grad_input = grad_output * forward_mask * backward_mask
-        return grad_input
-
-
-class GuidedBackPropagationReLU(nn.Module):
-    def __init__(self, **kwargs):
-        super(GuidedBackPropagationReLU, self).__init__(**kwargs)
-
-    def forward(self, x):
-        return _GuidedBackPropagationReLUFunction().forward(x)
-
-
-def replace_relu(m):
-    """
-    替换m中所有的ReLU为GuidedBackPropagationReLU
-    :param m: module
-    :return:
-    """
-    if len(m._modules) == 0:
-        return
-
-    for name, module in m._modules.items():
-        if len(module._modules) > 0:
-            replace_relu(module)
-        elif isinstance(module, nn.ReLU):  # module是最基础的layer
-            if isinstance(m, nn.Sequential):
-                m[int(name)] = GuidedBackPropagationReLU()
-            elif isinstance(m, nn.ModuleList):
-                m[int(name)] = GuidedBackPropagationReLU()
-            elif isinstance(m, nn.ModuleDict):
-                m[name] = GuidedBackPropagationReLU()
-            elif hasattr(m, name):
-                setattr(m, name, GuidedBackPropagationReLU())
-
-
 class GuidedBackPropagation(object):
+
     def __init__(self, net):
         self.net = net
+        for (name, module) in self.net.named_modules():
+            if isinstance(module, nn.ReLU):
+                module.register_backward_hook(self.backward_hook)
         self.net.eval()
+
+    @classmethod
+    def backward_hook(cls, module, grad_in, grad_out):
+        """
+
+        :param module:
+        :param grad_in: tuple,长度为1
+        :param grad_out: tuple,长度为1
+        :return: tuple(new_grad_in,)
+        """
+        return torch.clamp(grad_in[0], min=0.0),
 
     def __call__(self, inputs, index=None):
         """
@@ -72,10 +37,12 @@ class GuidedBackPropagation(object):
         :param index: class_id
         :return:
         """
+        inputs.grad.zero_()
         output = self.net(inputs)  # [1,num_classes]
         if index is None:
             index = np.argmax(output.cpu().data.numpy())
         target = output[0][index]
+
         target.backward()
 
-        return inputs.grad
+        return inputs.grad[0]  # [3,H,W]
