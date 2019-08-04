997899b1ca44130ef70307d7943eaeaae65b7bf5
yizt <csuyzt@163.com>
2019-08-04 10:29:19 +0800

    增加入口类

--- main.py
+++ main.py
@@ -4,4 +4,64 @@ Created on 2019/8/4 上午9:53
 
 @author: mick.yi
 
+入口类
+
 """
+from torch import nn
+from torchvision import models
+import argparse
+
+
+def get_net(net_name, weight_path=None):
+    """
+
+    :param net_name: 网络名称
+    :param weight_path: 与训练权重路径
+    :return:
+    """
+    pretrain = weight_path is None  # 没有指定权重路径，则加载默认的预训练权重
+    if net_name in ['vgg', 'vgg16']:
+        net = models.vgg16(pretrained=pretrain)
+    elif net_name in ['resnet', 'resnet50']:
+        net = models.resnet50(pretrained=pretrain)
+    elif net_name in ['densenet', 'densenet121']:
+        net = models.densenet121(pretrained=pretrain)
+    elif net_name in ['inception']:
+        net = models.inception_v3(pretrained=pretrain)
+    else:
+        raise ValueError('invalid network name:{}'.format(net_name))
+    return net
+
+
+def get_last_conv_name(net):
+    """
+
+    :param net: 获取网络的最后一个卷积层的名字
+    :return:
+    """
+    layer_name = None
+    for name, m in net.named_modules():
+        if isinstance(m, nn.Conv2d):
+            layer_name = name
+    return layer_name
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--network', type=str, default='resnet50',
+                        help='ImageNet classification network')
+    parser.add_argument('--image-path', type=str, default='./examples/pic1.png',
+                        help='input image path')
+    parser.add_argument('--weight-path', type=str, default=None,
+                        help='weight path of the model')
+    parser.add_argument('--layer-name', type=str, default=None,
+                        help='last convolutional layer name')
+    parser.add_argument('--class-id', type=int, default=None,
+                        help='class id')
+    arguments = parser.parse_args()
+
+    main(arguments)
