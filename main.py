9ac63f56fead16ba5dff8a69cc2c24e4d0fec07a
yizt <csuyzt@163.com>
2019-08-04 12:51:59 +0800

    完善grad-cam

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ Created on 2019/8/4 上午9:53
 入口类
 
 """
+import numpy as np
+import torch
 from torch import nn
 from torchvision import models
 import argparse
+# import matplotlib.pyplot as plt
+from skimage import io
+import cv2
+from interpretability.grad_cam import GradCAM
+from interpretability.guided_back_propagation import GuidedBackPropagation
 
 
 def get_net(net_name, weight_path=None):
@@ -30,13 +37,16 @@ def get_net(net_name, weight_path=None):
         net = models.inception_v3(pretrained=pretrain)
     else:
         raise ValueError('invalid network name:{}'.format(net_name))
+    # 加载指定路径的权重参数
+    if weight_path is not None:
+        net.load_state_dict(torch.load(weight_path))
     return net
 
 
 def get_last_conv_name(net):
     """
-
-    :param net: 获取网络的最后一个卷积层的名字
+    获取网络的最后一个卷积层的名字
+    :param net:
     :return:
     """
     layer_name = None
@@ -46,8 +56,52 @@ def get_last_conv_name(net):
     return layer_name
 
 
+def prepare_image(image):
+    image = image.copy()
+
+    # 归一化
+    means = np.array([0.485, 0.456, 0.406])
+    stds = np.array([0.229, 0.224, 0.225])
+    image -= means
+    image /= stds
+
+    image = np.ascontiguousarray(np.transpose(image, (2, 0, 1)))  # channel first
+    image = torch.from_numpy(image)
+    image.unsqueeze_(0)  # 增加batch维
+    image = torch.tensor(image, requires_grad=True)
+    return image
+
+
+def save_cam(image, mask):
+    """
+    保存CAM图
+    :param image: [H,W,C],原始图像
+    :param mask: [H,W],范围0~1
+    :return:
+    """
+    # mask转为heatmap
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    heatmap = heatmap[..., ::-1]  # gbr to rgb
+    io.imsave('heatmap.jpg',heatmap)
+    # 合并heatmap到原始图像
+    cam = heatmap + np.float32(image)
+    cam = cam / np.max(cam)
+    io.imsave("cam.jpg", np.uint8(255 * cam))
+
+
 def main(args):
-    pass
+    img = io.imread(args.image_path)
+    img = np.float32(cv2.resize(img, (224, 224))) / 255
+    inputs = prepare_image(img)
+
+    net = get_net(args.network, args.weight_path)
+
+    layer_name = get_last_conv_name(net) if args.layer_name is None else args.layer_name
+    grad_cam = GradCAM(net, layer_name)
+
+    mask = grad_cam(inputs, args.class_id)
+    save_cam(img, mask)
 
 
 if __name__ == '__main__':
