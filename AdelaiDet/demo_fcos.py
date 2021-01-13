d1ae4d6912fe74dafe0af20fbc532ee9ae89fe60
yizuotian <csuyzt@163.com>
2021-01-13 15:50:41 +0800

    fcos网络grad-cam

---
+++ AdelaiDet/demo_fcos.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : demo_fcos.py
+ @Time    : 2021/1/12 上午11:56
+ @Author  : yizuotian
+ @Description    :
+"""
+import argparse
+import multiprocessing as mp
+import os
+
+import cv2
+import detectron2.data.transforms as T
+import numpy as np
+import torch
+from detectron2.checkpoint import DetectionCheckpointer
+# from detectron2.config import get_cfg
+from adet.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.data.detection_utils import read_image
+from detectron2.modeling import build_model
+from detectron2.utils.logger import setup_logger
+from skimage import io
+
+from grad_cam_fcos import GradCAM
+
+# constants
+WINDOW_NAME = "COCO detections"
+
+
+def setup_cfg(args):
+    # load config from file and command-line arguments
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    # Set score_threshold for builtin models
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.FCOS.INFERENCE_TH_TEST = args.confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+
+    cfg.freeze()
+    return cfg
+
+
+def norm_image(image):
+    """
+    标准化图像
+    :param image: [H,W,C]
+    :return:
+    """
+    image = image.copy()
+    image -= np.max(np.min(image), 0)
+    image /= np.max(image)
+    image *= 255.
+    return np.uint8(image)
+
+
+def gen_cam(image, mask):
+    """
+    生成CAM图
+    :param image: [H,W,C],原始图像
+    :param mask: [H,W],范围0~1
+    :return: tuple(cam,heatmap)
+    """
+    print('max(mask):{}'.format(np.max(mask)))
+    # mask转为heatmap
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    print('max(heatmap):{}'.format(np.max(heatmap)))
+    # heatmap = np.float32(heatmap) / 255
+    heatmap = heatmap[..., ::-1]  # bgr to rgb
+    print('max(image):{},type(image)'.format(np.max(image)), type(image))
+    # 合并heatmap到原始图像
+    cam = heatmap + np.float32(image)
+    return norm_image(cam), heatmap
+
+
+def save_image(image_dicts, input_image_name, layer_name, network='fcos', output_dir='./results'):
+    prefix = os.path.splitext(input_image_name)[0]
+    for key, image in image_dicts.items():
+        if key == 'predict_box':
+            io.imsave(os.path.join(output_dir,
+                                   '{}-{}-{}.jpg'.format(prefix, network, key)),
+                      image)
+        else:
+            io.imsave(os.path.join(output_dir,
+                                   '{}-{}-{}-{}.jpg'.format(prefix, network, layer_name, key)),
+                      image)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin models")
+    parser.add_argument(
+        "--config-file",
+        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument("--input", help="A list of space separated input images")
+    parser.add_argument(
+        "--output",
+        help="A file or directory to save output visualizations. "
+             "If not given, will show output in an OpenCV window.",
+    )
+
+    parser.add_argument(
+        "--confidence-threshold",
+        type=float,
+        default=0.5,
+        help="Minimum score for instance predictions to be shown",
+    )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options using the command-line 'KEY VALUE' pairs",
+        default=[],
+        nargs=argparse.REMAINDER,
+    )
+    parser.add_argument('--layer-name', type=str, default='head.cls_subnet.2',
+                        help='使用哪层特征去生成CAM')
+    return parser
+
+
+def main(args):
+    setup_logger(name="fvcore")
+    logger = setup_logger()
+    logger.info("Arguments: " + str(args))
+
+    cfg = setup_cfg(args)
+    print(cfg)
+    # 构建模型
+    model = build_model(cfg)
+    print(model)
+    for name, module in model.named_modules():
+        print(name)
+    # 加载权重
+    checkpointer = DetectionCheckpointer(model)
+    checkpointer.load(cfg.MODEL.WEIGHTS)
+
+    # 加载图像
+    path = os.path.expanduser(args.input)
+    original_image = read_image(path, format="BGR")
+    height, width = original_image.shape[:2]
+    transform_gen = T.ResizeShortestEdge(
+        [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+    )
+    image = transform_gen.get_transform(original_image).apply_image(original_image)
+    image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1)).requires_grad_(True)
+
+    inputs = {"image": image, "height": height, "width": width}
+
+    # Grad-CAM
+    layer_name = args.layer_name
+    grad_cam = GradCAM(model, layer_name)
+    mask, box, class_id = grad_cam(inputs)  # cam mask
+    grad_cam.remove_handlers()
+
+    #
+    image_dict = {}
+    img = original_image[..., ::-1]
+    x1, y1, x2, y2 = box
+    image_dict['predict_box'] = img[y1:y2, x1:x2]
+    image_cam, image_dict['heatmap'] = gen_cam(img[y1:y2, x1:x2], mask[y1:y2, x1:x2])
+    # image_cam, image_dict['heatmap'] = gen_cam(img, mask)
+
+    save_image(image_dict, os.path.basename(path), args.layer_name)
+
+    # 获取类别名称
+    meta = MetadataCatalog.get(
+        cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
+    )
+    label = meta.thing_classes[class_id]
+
+    print("label:{}".format(label))
+
+
+if __name__ == "__main__":
+    """
+    Usage:export KMP_DUPLICATE_LIB_OK=TRUE
+    python AdelaiDet/demo_fcos.py --config-file AdelaiDet/R_50_1x.yaml \
+      --input ./examples/pic1.jpg \
+      --layer-name proposal_generator.fcos_head.cls_tower.8 \
+      --opts MODEL.WEIGHTS /sdb/tmp/pretrained_model/fcos_R_50_1x.pth MODEL.DEVICE cuda
+    """
+    mp.set_start_method("spawn", force=True)
+    arguments = get_parser().parse_args()
+    main(arguments)
---
+++ AdelaiDet/grad_cam_fcos.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : grad_cam_fcos.py
+ @Time    : 2021/1/12 下午1:53
+ @Author  : yizuotian
+ @Description    :
+"""
+import cv2
+import numpy as np
+
+
+class GradCAM(object):
+    """
+    1: 网络不更新梯度,输入需要梯度更新
+    2: 使用目标类别的得分做反向传播
+    """
+
+    def __init__(self, net, layer_name):
+        self.net = net
+        self.layer_name = layer_name
+        self.feature = []
+        self.gradient = []
+        self.net.eval()
+        self.handlers = []
+        self._register_hook()
+
+    def _get_features_hook(self, module, input, output):
+        self.feature.append(output)  # 不同层级特征
+        print("feature shape:{}".format(output.size()))
+
+    def _get_grads_hook(self, module, input_grad, output_grad):
+        """
+
+        :param input_grad: tuple, input_grad[0]: None
+                                   input_grad[1]: weight
+                                   input_grad[2]: bias
+        :param output_grad:tuple,长度为1
+        :return:
+        """
+        self.gradient.insert(0, output_grad[0])  # 梯度的顺序反的
+        print("gradient shape:{}".format(output_grad[0].size()))
+
+    def _register_hook(self):
+        for (name, module) in self.net.named_modules():
+            if name == self.layer_name:
+                self.handlers.append(module.register_forward_hook(self._get_features_hook))
+                self.handlers.append(module.register_backward_hook(self._get_grads_hook))
+
+    def remove_handlers(self):
+        for handle in self.handlers:
+            handle.remove()
+
+    def __call__(self, inputs, index=0):
+        """
+
+        :param inputs: {"image": [C,H,W], "height": height, "width": width}
+        :param index: 第几个边框
+        :return:
+        """
+        self.net.zero_grad()
+        output = self.net.forward([inputs])
+        print(output)
+        score = output[0]['instances'].scores[index]
+        feature_level = output[0]['instances'].fpn_levels[index]  # box来自第几层feature map
+        score.backward()
+
+        gradient = self.gradient[feature_level][0].cpu().data.numpy()  # [C,H,W]
+        weight = np.mean(gradient, axis=(1, 2))  # [C]
+        # feature_level 指feature map的层级，0去除batch维
+        feature = self.feature[feature_level][0].cpu().data.numpy()  # [C,H,W]
+        cam = feature * weight[:, np.newaxis, np.newaxis]  # [C,H,W]
+
+        cam = np.sum(cam, axis=0)  # [H,W]
+        print('cam:{},{},{}'.format(np.std(cam), np.max(cam), np.min(cam)))
+        cam = np.maximum(cam, 0) if np.max(cam) > 0 else cam  # ReLU, 如果最大值小于0，不做任何事
+
+        # 数值归一化
+        cam -= np.min(cam)
+        cam /= np.max(cam)
+        # 缩放到输入图像尺寸
+        h, w = inputs['height'], inputs['width']
+        cam = cv2.resize(cam, (w, h))
+
+        box = output[0]['instances'].pred_boxes.tensor[index].detach().cpu().numpy().astype(np.int32)
+        class_id = output[0]['instances'].pred_classes[index].detach().cpu().numpy()
+        return cam, box, class_id
